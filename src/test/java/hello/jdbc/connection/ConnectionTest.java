7e38fe046901d3cb5fa8f40430f8544b6e9104d8
ZENO <zeno1030@naver.com>
2023-12-14 17:34:01 +0900

    트랜잭션 문제ì 적용1

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,6 +1,6 @@
 package hello.jdbc.connection;
 
-import lombok.Data;
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -26,6 +26,22 @@ public class ConnectionTest {
     void dataSourceDriverManager() throws SQLException {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //케넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+
+
     }
     private void useDataSource(DataSource dataSource) throws SQLException {
         Connection con1 = dataSource.getConnection();
---
+++ src/test/java/hello/jdbc/service/MemberServiceV1Test.java
@@ -0,0 +1,78 @@
+package hello.jdbc.service;
+
+import hello.jdbc.connection.ConnectionConst;
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * 기본 동자, 트랜잭션이 없어서 문제 발생
+ */
+class MemberServiceV1Test {
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_C = "ex";
+
+    private MemberRepositoryV1 memberRepositoryV1;
+    private MemberServiceV1 memberServiceV1;
+    @BeforeEach
+    void before(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepositoryV1 = new MemberRepositoryV1(dataSource);
+        memberServiceV1 = new MemberServiceV1(memberRepositoryV1);
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepositoryV1.delete(MEMBER_A);
+        memberRepositoryV1.delete(MEMBER_B);
+        memberRepositoryV1.delete(MEMBER_C);
+    }
+    
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException {
+        //given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepositoryV1.save(memberA);
+        memberRepositoryV1.save(memberB);
+        //when
+        memberServiceV1.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+        //then
+        Member findMemberA = memberRepositoryV1.findById(memberA.getMemberId());
+        Member findMemberB = memberRepositoryV1.findById(memberB.getMemberId());
+        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    @DisplayName("이체중 예외 발생")
+    void accountTransferEx() throws SQLException {
+        //given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberC = new Member(MEMBER_C, 10000);
+        memberRepositoryV1.save(memberA);
+        memberRepositoryV1.save(memberC);
+        //when
+        assertThatThrownBy(() -> memberServiceV1.accountTransfer(memberA.getMemberId(), memberC.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+        //then
+        Member findMemberA = memberRepositoryV1.findById(memberA.getMemberId());
+        Member findMemberB = memberRepositoryV1.findById(memberC.getMemberId());
+        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberB.getMoney()).isEqualTo(10000);
+    }
+}
