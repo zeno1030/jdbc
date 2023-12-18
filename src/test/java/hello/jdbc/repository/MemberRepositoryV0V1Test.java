cd9de19abd4924b008edc41e97385d4d300e9960
ZENO <zeno1030@naver.com>
2023-12-18 14:34:27 +0900

    예외처리, 반복,jdbcTemplate사용

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0V1Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0V1Test.java
@@ -1,9 +1,15 @@
 package hello.jdbc.repository;
 
+import com.zaxxer.hikari.HikariDataSource;
+import hello.jdbc.connection.ConnectionConst;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import java.net.ConnectException;
+import java.net.URL;
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
@@ -11,9 +17,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
-class MemberRepositoryTest {
-    MemberRepository repository = new MemberRepository();
-    @Test
+class MemberRepositoryV0V1Test {
+
+    MemberRepositoryV1 repository;
+    @BeforeEach
+    void beforeEach() {
+        //기본 DriverManager - 항상 새로운 커넥션을 획득
+        //DriverManagerDataSource dataSource = new DriverManagerDataSource(ConnectionConst.URL, ConnectionConst.USERNAME, ConnectionConst.PASSWORD);
+
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(ConnectionConst.URL);
+        dataSource.setUsername(ConnectionConst.USERNAME);
+        dataSource.setPassword(ConnectionConst.PASSWORD);
+        dataSource.setPoolName("HikariPool");
+
+        repository = new MemberRepositoryV1(dataSource);
+
+    }
+        @Test
     void crud() throws SQLException {
         Member member = new Member("memberV100", 10000);
         repository.save(member);
